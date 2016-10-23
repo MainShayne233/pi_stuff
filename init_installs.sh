372c3044be0c7dc4f643d6d1b4b845f0f1d4b5d0
MainShayne233 <shaynetremblay@hotmail.com>
2016-10-22 23:46:46 -0400

    updateed

--- init_installs.sh
+++ init_installs.sh
@@ -1,2 +1,17 @@
 #!/bin/bash
+sudo apt-get update
+
 sudo apt-get install vim git postgresql -y
+
+sudo apt-get install wget -y
+sudo apt-get install libssl-dev -y
+sudo apt-get install ncurses-dev -y
+wget http://www.erlang.org/download/otp_src_19.1.tar.gz
+tar -xzvf otp_src_19.1.tar.gz
+cd otp_src_19.1/
+./configure
+make
+sudo make install
+cd ..
+rm otp_src_19.1.tar.gz
+sudo rm -R otp_src_19.1/
